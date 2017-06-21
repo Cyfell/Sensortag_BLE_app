078e8fe287ee2ae0706690c2f94657e9883b4163
Arthur Courtel <arthur.courtel@openwide.fr>
2017-06-21 09:43:18 +0200

    Add BLE scan in listview

--- app/src/main/java/fr/smile/sensortag_ble/BLE_scan.java
+++ app/src/main/java/fr/smile/sensortag_ble/BLE_scan.java
@@ -1,26 +1,38 @@
 package fr.smile.sensortag_ble;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BLE_scan extends AppCompatActivity {
     private BluetoothAdapter mBluetoothAdapter;
-
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> defaultArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ble_scan);
-
+        defaultArray = new ArrayList<String>();
+        adapter = new <String>ArrayAdapter(this, android.R.layout.simple_list_item_1, defaultArray);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
         init_Bluetooth_Adapter();
+
+        scan_le_device();
 
 
     }
-    
+
     private void init_Bluetooth_Adapter(){
         // Initializes Bluetooth adapter.
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
@@ -35,6 +47,29 @@ public class BLE_scan extends AppCompatActivity {
             Toast toast = Toast.makeText(getApplicationContext(), "Yolo", Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
+
+    private void scan_le_device(){
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+    }
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi,
+                                     byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            defaultArray.add(device.getAddress());
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+                }
+            };
+
+    private void UpdateListView(BluetoothDevice device) {
+
     }
 
 
